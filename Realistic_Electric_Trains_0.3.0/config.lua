9270e5706514b8ccb9402de4a5f82009e5bc6ad5
Julian <julisch1107@web.de>
2019-03-10 22:11:25 +0100

    Cleaning up tick handler and reducing pole energy buffer.

--- Realistic_Electric_Trains_0.3.0/config.lua
+++ Realistic_Electric_Trains_0.3.0/config.lua
@@ -2,8 +2,6 @@
 
 local ticks_per_update = settings.startup["ret-ticks-per-update"].value
 
--- 2.4MW
-local loco_max_supported_power = 2400000
 -- 600kW (vanilla)
 local locomotive_power =   600000
 -- 1.2MW (2* vanilla)
@@ -17,10 +15,10 @@ config = {
 	pole_max_wire_distance = 16,
 	pole_supply_area = 4,
 
-	-- Supports 8 times the maximum loco power per pole buffer
-	pole_power_buffer = store(8 * loco_max_supported_power),
-	-- Refills the energy of two locos until the next update
-	pole_flow_limit = 2 * loco_max_supported_power,
+	-- Supports a continuous supply of 4.8MW
+	pole_power_buffer = store(4800000),
+	-- Refills the buffer until the next update
+	pole_flow_limit = 4800000,
 
 	-- Locomotive Mk 1
 	locomotive_power = locomotive_power,
--- Realistic_Electric_Trains_0.3.0/logic/events/on_tick.lua
+++ Realistic_Electric_Trains_0.3.0/logic/events/on_tick.lua
@@ -9,66 +9,75 @@ do
 	function on_tick(event)
 
 		if not loco_fuel_data then
-			local loco_1_prototype = game.entity_prototypes["ret-electric-locomotive"]
-			local loco_1_efficiency_modifier = 1.05
-			if loco_1_prototype.burner_prototype.effectivity < 1 then
-				loco_1_efficiency_modifier = loco_1_efficiency_modifier / loco_1_prototype.burner_prototype.effectivity
-			end
-			
-			local loco_2_prototype = game.entity_prototypes["ret-electric-locomotive-mk2"]
-			local loco_2_efficiency_modifier = 1.05
-			if loco_2_prototype.burner_prototype.effectivity < 1 then
-				loco_2_efficiency_modifier = loco_2_efficiency_modifier / loco_2_prototype.burner_prototype.effectivity
-			end
-			
-			loco_fuel_data = {
-				["ret-electric-locomotive"] = {
-					item = game.item_prototypes["ret-dummy-fuel-1"],
-					fuel = game.item_prototypes["ret-dummy-fuel-1"].fuel_value,
-					-- max_energy_usage is in J/s instead of J/tick as in data
-					transfer = loco_1_prototype.max_energy_usage * ticks_per_update * loco_1_efficiency_modifier,
-				},
-				["ret-electric-locomotive-mk2"] = {
-					item = game.item_prototypes["ret-dummy-fuel-2"],
-					fuel = game.item_prototypes["ret-dummy-fuel-2"].fuel_value,
-					-- max_energy_usage is in J/s instead of J/tick as in data
-					transfer = loco_2_prototype.max_energy_usage * ticks_per_update * loco_2_efficiency_modifier,
-				}
-			}
+			setup_fuel_data()
 		end
-
 
 		-- Power all locomotives
 		local offset = event.tick % ticks_per_update
 		local locos = global.electric_locos
 
 		for i = #locos - offset, 1, -ticks_per_update do
-			local loco = locos[i]
-			local burner = loco.burner
-
-			local fuel_data = loco_fuel_data[loco.name]
-
-			if burner.remaining_burning_fuel <= 0 then
-				burner.currently_burning = fuel_data.item
-			end
-
-			local missing_energy = fuel_data.fuel - burner.remaining_burning_fuel
-			local power_provider = find_power_provider(loco)
-
-			if power_provider then
-				local transfer = math.min(missing_energy,
-								 math.min(power_provider.energy, fuel_data.transfer))
-				if transfer > 0 then
-					burner.remaining_burning_fuel =
-							burner.remaining_burning_fuel + transfer
-					power_provider.energy =
-							power_provider.energy - transfer
-				end
-			end
-		end -- for loop
-
+			update_locomotive(locos[i])
+		end
 
 	end
+
+
+
+	function setup_fuel_data()
+		local loco_1_prototype = game.entity_prototypes["ret-electric-locomotive"]
+		local loco_1_efficiency_modifier = 1.05
+		if loco_1_prototype.burner_prototype.effectivity < 1 then
+			loco_1_efficiency_modifier = loco_1_efficiency_modifier / loco_1_prototype.burner_prototype.effectivity
+		end
+		
+		local loco_2_prototype = game.entity_prototypes["ret-electric-locomotive-mk2"]
+		local loco_2_efficiency_modifier = 1.05
+		if loco_2_prototype.burner_prototype.effectivity < 1 then
+			loco_2_efficiency_modifier = loco_2_efficiency_modifier / loco_2_prototype.burner_prototype.effectivity
+		end
+		
+		loco_fuel_data = {
+			["ret-electric-locomotive"] = {
+				item = game.item_prototypes["ret-dummy-fuel-1"],
+				fuel = game.item_prototypes["ret-dummy-fuel-1"].fuel_value,
+				-- max_energy_usage is in J/t instead of J/s as in data
+				transfer = loco_1_prototype.max_energy_usage * ticks_per_update * loco_1_efficiency_modifier,
+			},
+			["ret-electric-locomotive-mk2"] = {
+				item = game.item_prototypes["ret-dummy-fuel-2"],
+				fuel = game.item_prototypes["ret-dummy-fuel-2"].fuel_value,
+				-- max_energy_usage is in J/t instead of J/s as in data
+				transfer = loco_2_prototype.max_energy_usage * ticks_per_update * loco_2_efficiency_modifier,
+			}
+		}
+	end
+
+
+	function update_locomotive(loco)
+		local burner = loco.burner
+		local fuel_data = loco_fuel_data[loco.name]
+
+		if burner.remaining_burning_fuel <= 0 then
+			burner.currently_burning = fuel_data.item
+		end
+
+		local missing_energy = fuel_data.fuel - burner.remaining_burning_fuel
+		local power_provider = find_power_provider(loco)
+
+		if power_provider then
+			local transfer = math.min(missing_energy,
+							 math.min(power_provider.energy, fuel_data.transfer))
+			if transfer > 0 then
+				burner.remaining_burning_fuel =
+						burner.remaining_burning_fuel + transfer
+				power_provider.energy =
+						power_provider.energy - transfer
+			end
+		end
+	end
+
 end
+
 
 return on_tick
