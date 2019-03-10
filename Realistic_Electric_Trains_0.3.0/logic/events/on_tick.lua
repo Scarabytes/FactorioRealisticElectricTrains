95330174ac0a1d4541198baa6319a557f82f9f01
Optera <yousei.kyoko+github@gmail.com>
2019-03-10 08:25:35 +0100

    fixed energy transfer
    - read locomotive power from prototype
    - account for burner.efficiency

--- Realistic_Electric_Trains_0.3.0/logic/events/on_tick.lua
+++ Realistic_Electric_Trains_0.3.0/logic/events/on_tick.lua
@@ -9,16 +9,30 @@ do
 	function on_tick(event)
 
 		if not loco_fuel_data then
+			local loco_1_prototype = game.entity_prototypes["ret-electric-locomotive"]
+			local loco_1_efficiency_modifier = 1.05
+			if loco_1_prototype.burner_prototype.effectivity < 1 then
+				loco_1_efficiency_modifier = loco_1_efficiency_modifier * (2 - loco_1_prototype.burner_prototype.effectivity)
+			end
+			
+			local loco_2_prototype = game.entity_prototypes["ret-electric-locomotive-mk2"]
+			local loco_2_efficiency_modifier = 1.05
+			if loco_2_prototype.burner_prototype.effectivity < 1 then
+				loco_2_efficiency_modifier = loco_2_efficiency_modifier * (2 - loco_2_prototype.burner_prototype.effectivity)
+			end
+			
 			loco_fuel_data = {
 				["ret-electric-locomotive"] = {
 					item = game.item_prototypes["ret-dummy-fuel-1"],
 					fuel = game.item_prototypes["ret-dummy-fuel-1"].fuel_value,
-					transfer = config.locomotive_power / 60 * ticks_per_update * 1.05
+					-- max_energy_usage is in J/s instead of J/tick as in data
+					transfer = loco_1_prototype.max_energy_usage * ticks_per_update * loco_1_efficiency_modifier,
 				},
 				["ret-electric-locomotive-mk2"] = {
 					item = game.item_prototypes["ret-dummy-fuel-2"],
 					fuel = game.item_prototypes["ret-dummy-fuel-2"].fuel_value,
-					transfer = config.locomotive2_power / 60 * ticks_per_update * 1.05
+					-- max_energy_usage is in J/s instead of J/tick as in data
+					transfer = loco_2_prototype.max_energy_usage * ticks_per_update * loco_2_efficiency_modifier,
 				}
 			}
 		end
@@ -42,7 +56,7 @@ do
 			local power_provider = find_power_provider(loco)
 
 			if power_provider then
-				local transfer = math.min(missing_energy, 
+				local transfer = math.min(missing_energy,
 								 math.min(power_provider.energy, fuel_data.transfer))
 				if transfer > 0 then
 					burner.remaining_burning_fuel =
