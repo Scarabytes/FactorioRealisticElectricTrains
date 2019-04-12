9bbbaac9e698814e536e1723157657085a1f2e43
robot256 <robot256@gmail.com>
2019-04-12 17:26:00 -0400

    Fix typo in register_locomotive_type

--- Realistic_Electric_Trains_0.4.1/logic/remotes.lua
+++ Realistic_Electric_Trains_0.4.1/logic/remotes.lua
@@ -11,7 +11,7 @@
 -- The latter enables modules being detected in the locomotive.
 function register_locomotive_type(loco_name, fuel_item)
 	if (not game.entity_prototypes[loco_name]) or 
-	   (fuel_item ~= nil and fuel_item ~= "ret-dummy-fuel-modular" and game.item_prototypes[fuel_item] ~= nil) then
+	   (fuel_item ~= nil and fuel_item ~= "ret-dummy-fuel-modular" and game.item_prototypes[fuel_item] == nil) then
 		error("register_locomotive_type called with invalid arguments")
 	end
 	global.electric_loco_registry[loco_name] = fuel_item
